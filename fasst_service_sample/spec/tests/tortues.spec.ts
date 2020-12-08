6d32df16abfebcb7416d7f5c18aafaa4a73f53dd
mariakazakova <75610158+mariakazakova@users.noreply.github.com>
2020-12-08 15:01:51 +0100

    first test

--- fasst_service_sample/spec/tests/tortues.spec.ts
+++ fasst_service_sample/spec/tests/tortues.spec.ts
@@ -1,13 +1,27 @@
 import app from '@server';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import { ITortue, tortue } from '@entities/Tortue';
 import 'mocha';
 
 chai.use(chaiHttp);
 
-describe('Get API Request', () => {
-	it('should return response on call', async () => {
-	    const res = await chai.request(app).get('/');
-	    console.log(res);
+describe('hooks', async () => {
+
+    beforeEach(async () => {
+      await tortue.deleteMany({});
+      await tortue.create({
+            "name": "Toto",
+            "age": 17,
+            "taille": 19,
+            "terrestre": false,
+            "species": "Testudo (Agrionemys) horsfieldii"
+        });
+    });
+
+    it('should return response on call', async () => {       
+	    const res = await chai.request(app).get('/api/tortues');
+	    console.log("my response", res);
 	});
+  
 });
--- fasst_service_sample/src/index.ts
+++ fasst_service_sample/src/index.ts
@@ -1,8 +1,7 @@
 import './preStart'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
-const express = require('express');
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 mongoose.connect("mongodb://localhost/tortues");
 
 const port = Number(process.env.PORT || 3000);
