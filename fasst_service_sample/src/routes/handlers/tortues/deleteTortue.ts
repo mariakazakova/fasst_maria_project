8353635fcd1ccf94ae37ef55e783eb7cb26c57ce
mariakazakova <75610158+mariakazakova@users.noreply.github.com>
2020-12-08 11:25:59 +0100

    changes

--- fasst_service_sample/src/routes/handlers/tortues/deleteTortue.ts
+++ fasst_service_sample/src/routes/handlers/tortues/deleteTortue.ts
@@ -9,6 +9,7 @@ export const deleteTortue = async (req: Request, res: Response) => {
 		await turtleDao.delete(Number(turtleId));
 =======
 		await turtleDao.delete(turtleId);
+		res.status(StatusCodes.OK);
 >>>>>>> Stashed changes
 	} catch (e) {
 		res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ error: e.message });
