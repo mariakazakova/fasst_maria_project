c33f19a1b1945a8f164900b802c1329f9ac23beb
mariakazakova <75610158+mariakazakova@users.noreply.github.com>
2020-12-08 16:17:02 +0100

    changes

--- fasst_service_sample/spec/tests/tortues.spec.ts
+++ fasst_service_sample/spec/tests/tortues.spec.ts
@@ -10,19 +10,19 @@ chai.use(chaiHttp);
 describe('hooks', async () => {
     let request: ChaiHttp.Agent;
     const testTortues = [{
-			'name': 'Toto',
-			'age': 11,
-			'taille': 19,
-			'terrestre': false,
-			'species': 'Trachemys scripta'
-        },
-        {
-			'name': 'Tortilla',
-			'age': 25,
-			'taille': 150,
-			'terrestre': true,
-			'species': 'Testudo (Agrionemys) horsfieldii'
-		}
+        'name': 'Toto',
+        'age': 11,
+        'taille': 19,
+        'terrestre': false,
+        'species': 'Trachemys scripta'
+    },
+    {
+        'name': 'Tortilla',
+        'age': 25,
+        'taille': 150,
+        'terrestre': true,
+        'species': 'Testudo (Agrionemys) horsfieldii'
+    }
     ]
 
     const tortueToAdd = {
@@ -33,33 +33,33 @@ describe('hooks', async () => {
         'species': 'Trachemys scripta'
     }
 
-	before(async () => {
-		const app = await init();
-		request = chai.request(app);
-	});
+    before(async () => {
+        const app = await init();
+        request = chai.request(app);
+    });
 
-	beforeEach(async () => {
-		await tortue.deleteMany();
-		await tortue.create(testTortues[0]);
+    beforeEach(async () => {
+        await tortue.deleteMany();
+        await tortue.create(testTortues[0]);
         await tortue.create(testTortues[1]);
-	});
+    });
 
-	it('GET api/tortues all tortues', async () => {
-	    const response = await request.get('/api/tortues');
+    it('GET api/tortues all tortues', async () => {
+        const response = await request.get('/api/tortues');
         const data = response.body;
         assert(Array.isArray(data), "data is returned as array");
-        assert(data.length==testTortues.length, "number of tortles is correct");
+        assert(data.length == testTortues.length, "number of tortles is correct");
         data.forEach((value: ITortue, i: number) => {
-            assert(value.name===testTortues[i].name, "le nom de tortue est correcte");
-            assert(value.age===testTortues[i].age, "l'age de tortue est correcte");
-            assert(value.taille===testTortues[i].taille, "la taille de tortue est correcte");
-            assert(value.terrestre===testTortues[i].terrestre, "l'habitat de tortue est correcte");
-            assert(value.species===testTortues[i].species, "le species de tortue est correcte");
+            assert(value.name === testTortues[i].name, "le nom de tortue est correcte");
+            assert(value.age === testTortues[i].age, "l'age de tortue est correcte");
+            assert(value.taille === testTortues[i].taille, "la taille de tortue est correcte");
+            assert(value.terrestre === testTortues[i].terrestre, "l'habitat de tortue est correcte");
+            assert(value.species === testTortues[i].species, "le species de tortue est correcte");
         });
     });
 
     it('POST api/tortues create tortue', async () => {
-	    const response = await request.post('/api/tortues').set('content-type', 'application/json').send(tortueToAdd);
+        const response = await request.post('/api/tortues').set('content-type', 'application/json').send(tortueToAdd);
         const data = response.body;
         console.log("after post ", data);
     });
