f462daa69a39f7914b77e88328d74e54754ba1d1
FasstLoic <57351368+FasstLoic@users.noreply.github.com>
2020-12-09 14:38:07 +0100

    wip

--- fasst_service_sample/src/Server.ts
+++ fasst_service_sample/src/Server.ts
@@ -20,23 +20,13 @@ const init = async () => {
 /************************************************************************************
  *                              Set basic express settings
  ***********************************************************************************/
-	const mongoDBUrl = 'mongodb://db:27017/tortues';
-	await mongoose.connect('mongodb://db:27017/tortues',  { useNewUrlParser: true, useUnifiedTopology: true });
+	const mongoDBUrl = pathOr('mongodb://db:27017/tortues');
+	await mongoose.connect(mongoDBUrl,  { useNewUrlParser: true, useUnifiedTopology: true });
 
 	console.log(`Connected at ${mongoDBUrl}`);
 	app.use(express.json());
 	app.use(express.urlencoded({extended: true}));
 	app.use(cookieParser());
-
-	// Show routes called in console during development
-	if (process.env.NODE_ENV === 'development') {
-		app.use(morgan('dev'));
-	}
-
-	// Security
-	if (process.env.NODE_ENV === 'production') {
-		app.use(helmet());
-	}
 
 	// Add APIs
 	app.use('/api', BaseRouter);
