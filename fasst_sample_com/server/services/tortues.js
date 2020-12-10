03a70e29db0ca6422986c9d943e053e9be2a1285
mariakazakova <75610158+mariakazakova@users.noreply.github.com>
2020-12-10 11:11:30 +0100

    Update tortues.js

--- fasst_sample_com/server/services/tortues.js
+++ fasst_sample_com/server/services/tortues.js
@@ -1,46 +1,46 @@
 import service from './service';
 
 const tortueService = () => (command, args) => {
-    return service(
-      'tortue',
-      process.env.TORTUES_SERVICE_URL,
-      {
-        getTortues: {
-          route: '/api/tortues',
-          method: 'GET'
-        },
-        getOneTortue: {
-          route: '/api/tortues/:id',
-          method: 'GET'
-        },
-        createTortue: {
-          route: '/api/tortues',
-          method: 'POST',
-          body: [
-            'name',
-            'age',
-            'taille',
-            'terrestre',
-            ['species', 'o']
-          ]
-        },
-        updateTortue: {
-          route: '/api/tortues/:id',
-          method: 'PUT',
-          body: [
-            'name',
-            'age',
-            'taille',
-            'terrestre',
-            ['species', 'o']
-          ]
-        },
-        deleteTortue: {
-          route: '/delete/:id',
-          method: 'DELETE'
-        }
+  return service(
+    'tortue',
+    process.env.TORTUES_SERVICE_URL,
+    {
+      getTortues: {
+        route: '/api/tortues',
+        method: 'GET'
+      },
+      getOneTortue: {
+        route: '/api/tortues/:id',
+        method: 'GET'
+      },
+      createTortue: {
+        route: '/api/tortues',
+        method: 'POST',
+        body: [
+          'name',
+          'age',
+          'taille',
+          'terrestre',
+          ['species', 'o']
+        ]
+      },
+      updateTortue: {
+        route: '/api/tortues/:id',
+        method: 'PUT',
+        body: [
+          'name',
+          'age',
+          'taille',
+          'terrestre',
+          ['species', 'o']
+        ]
+      },
+      deleteTortue: {
+        route: '/delete/:id',
+        method: 'DELETE'
       }
-    )(command, args);
-  };
+    }
+  )(command, args);
+};
   
-  module.exports = tortueService;
+module.exports = tortueService;
