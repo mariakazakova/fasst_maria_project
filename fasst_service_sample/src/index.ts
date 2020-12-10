bde7a70ceb838e5a39ac098882361c6a61cc44f1
fasstloic <loic.remongin@fasst.io>
2020-12-10 15:55:04 +0100

    fix configuration 3000 / 3001 remove invalid environment configuration

--- fasst_service_sample/src/index.ts
+++ fasst_service_sample/src/index.ts
@@ -1,8 +1,7 @@
-import './preStart'; // Must be the first import
 import init from '@server';
 import logger from '@shared/Logger';
 
-const port = Number(process.env.PORT || 3001);
+const port = Number(process.env.PORT || 3000);
 init().then(app =>
 	app.listen(port, () => {
 		logger.info('Express server started on port: ' + port);
