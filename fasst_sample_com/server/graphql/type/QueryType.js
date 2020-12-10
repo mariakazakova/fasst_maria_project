69174a1a2ef184d4e0b7eb53a8236a6f43069dfe
fasstloic <loic.remongin@fasst.io>
2020-12-10 16:10:35 +0100

    fix configuration

--- fasst_sample_com/server/graphql/type/QueryType.js
+++ fasst_sample_com/server/graphql/type/QueryType.js
@@ -1,9 +1,11 @@
 const { gql } = require('apollo-server-express');
+import TortueResolver from '../resolver/tortueResolver';
 
 module.exports = {
   typeDefs: gql`
     type Query {
       homepage: Home
+      getTortues: [Tortue]
     }
   `,
   resolvers: {
@@ -11,6 +13,7 @@ module.exports = {
       homepage: () => {
         return { message: 'Starter kit' };
       },
+      getTortues: (parent, args, context) => TortueResolver(context).getAllTortles()
     }
   }
 };
