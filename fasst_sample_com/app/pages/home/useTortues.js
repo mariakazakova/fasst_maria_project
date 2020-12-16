1711e01cbccb4751002ac77e1395a0705d6e79aa
mariakazakova <75610158+mariakazakova@users.noreply.github.com>
2020-12-16 09:04:16 +0100

    Update useTortues.js

--- fasst_sample_com/app/pages/home/useTortues.js
+++ fasst_sample_com/app/pages/home/useTortues.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import QTortlesMutation from '../../_graphql/mutations/QAddTortle';
+import { inc } from 'ramda';
 
 export const useTortues = () => {
   const [key, setKey] = useState(0);
 
   const createTortue = async (data) => {
+    console.log("in use tortue, key : ", key)
     QTortlesMutation({
       name: data.name,
       age: data.age,
@@ -13,7 +15,7 @@ export const useTortues = () => {
       species: data.species
     }, (hasError, data) => {
       if (!hasError) {
-        setKey(key + 1); //pas reussi à trouver de quel lib vient inc()
+        setKey(inc(key));
       }
     });
   };
